5be109073fb8c0b20ff7e9921cac905c6cb4ffac
BaritoneBeard <NickAlmeder@Yahoo.com>
2023-02-13 17:29:49 -0500

    updating gitnor and took out files that didn't need to be in the repo

--- programs/Domineering_SImulation/test.py
+++
@@ -1,9 +0,0 @@
-with open('graphs.txt', 'r') as document:
-    answer = {}
-    for line in document:
-        line = line.split()
-        if not line:
-            continue
-        answer[line[0]] = line[1:]
-
-print(answer)
