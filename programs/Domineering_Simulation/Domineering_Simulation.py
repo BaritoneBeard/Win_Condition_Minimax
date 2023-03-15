898485035da2cf57495b09c182a0aa3555d73e10
BaritoneBeard <NickAlmeder@Yahoo.com>
2023-03-15 10:23:43 -0400

    Implemented traditional minimax for comparison

--- programs/Domineering_Simulation/Domineering_Simulation.py
+++ programs/Domineering_Simulation/Domineering_Simulation.py
@@ -1,5 +1,5 @@
-from programs.Domineering_Simulation.helpers.graph_and_board_computation import *
 from programs.Domineering_Simulation.Minimax import *
+import programs.Domineering_Simulation.trad_Minimax as tm
 import time
 
 
@@ -10,10 +10,28 @@ def main():
     board = fill_board(file=file, board=board)
     build_graph_from_board(graph=graph, board=board)
     print_board(board)
+    depth = 5
+    player = -1
 
     delta = time.time()
-    possible_first_moves = Minimax(AI_player=-1, graph=graph, board=board, depth=5).winning_moves
-    print(time.time() - delta)
+    traditional_minimax = tm.Minimax(AI_player=player, graph=graph, board=board, depth=depth)
+    # trad_wins = tm.Minimax(AI_player=-1, graph=graph, board=board, depth=depth).found_end
+    traditional_minimax_time = time.time() - delta
+
+    delta = time.time()
+    wincon_minimax = Minimax(AI_player=player, graph=graph, board=board, depth=depth)
+    possible_first_moves = wincon_minimax.winning_moves
+    # wincon_wins = wincon_minimax.found_end
+    wincon_minimax_time = time.time() - delta
+
+    print(f"\nTime comparison at recursive depth: {depth}\n"
+          f"Traditional Minimax: {traditional_minimax_time} \n"
+          f"Win Condition Minimax: {wincon_minimax_time}")
+
+    # print(f"\nNumber of solved games: \n"
+    #       f"Traditional Minimax: {trad_wins}\n"
+    #       f"Win Condition Minimax: {wincon_wins}")
+
     filtered_list = {k for k in possible_first_moves if possible_first_moves[k] < 0}
     print(f"\nThe full list of moves and their values: {possible_first_moves} \n"
           f"List of moves which guarantee AI the win: {filtered_list}")
--- programs/Domineering_Simulation/trad_Minimax.py
+++ programs/Domineering_Simulation/trad_Minimax.py
@@ -1,4 +1,3 @@
-from programs.Domineering_Simulation.helpers.check_partition_existence import check_partition_existence
 from programs.Domineering_Simulation.helpers.check_graph_possibilities import *
 from programs.Domineering_Simulation.helpers.graph_and_board_computation import *
 
@@ -16,33 +15,24 @@ class Minimax:
         self.beta = 9999
         self.found_end = 0
 
-        self.minimax(self.player, self.graph, self.board, self.depth, self.alpha, self.beta)
+        self.traditional_minimax(self.player, self.graph, self.board, self.depth, self.alpha, self.beta)
 
-
-    def minimax(self, player, graph, board, depth, alpha, beta):
-
-        # The first Base Case: Stop if the board is partitioned
-        if check_partition_existence(graph, 'E', 'W') is True and check_partition_existence(graph, 'N', 'S') is True:
-            print("Partitioned!")
-            print_board(board)
-            self.found_end += 1
-            calculation = calc_possibilities(board)
-            self.winning_moves[str(self.first_move)] = calculation
-            return calculation
-
+    def traditional_minimax(self, player, graph, board, depth, alpha, beta):
         # Worst case base case: If no partitions are found, might as well return something
         if depth <= 0:
             return calc_possibilities(board)
 
-        # If depth > 0 and no partitions are found, propose a series of moves
         if player == 1:
             value = -9999
             possible_moves = possible_vertical_moves(board)
+            if len(possible_moves) == 0:
+                self.found_end += 1
+                return alpha
             for move in possible_moves:
                 if depth == self.depth:
                     self.first_move = move
                 copy, new_graph = create_copies(board=board, move=move)
-                value = max(value, self.minimax(player = player * -1, graph = new_graph, board = copy, depth = depth - 1, alpha = alpha, beta = beta))
+                value = max(value, self.traditional_minimax(player = player * -1, graph = new_graph, board = copy, depth = depth - 1, alpha = alpha, beta = beta))
                 alpha = max(value, alpha)
                 if beta <= alpha:
                     break
@@ -50,11 +40,14 @@ class Minimax:
         else:
             value = 9999
             possible_moves = possible_horizontal_moves(board)
+            if len(possible_moves) == 0:
+                self.found_end += 1
+                return beta
             for move in possible_moves:
                 if depth == self.depth:
                     self.first_move = move
                 copy, new_graph = create_copies(board=board, move=move)
-                value = min(value, self.minimax(player = player * -1, graph = new_graph, board = copy, depth = depth - 1, alpha = alpha, beta = beta))
+                value = min(value, self.traditional_minimax(player = player * -1, graph = new_graph, board = copy, depth = depth - 1, alpha = alpha, beta = beta))
                 beta = min(value, beta)
                 if beta <= alpha:
                     break
