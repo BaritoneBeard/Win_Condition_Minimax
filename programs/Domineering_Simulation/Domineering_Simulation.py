1cb30ad5fbb80a9f52ec276052b5e36e83edae22
BaritoneBeard <NickAlmeder@Yahoo.com>
2023-04-25 12:24:34 -0400

    Changes as suggested by Thesis Committee Member: Multiple tests have been added, we now have different ways to test different variables

--- programs/Domineering_Simulation/Domineering_Simulation.py
+++ programs/Domineering_Simulation/Domineering_Simulation.py
@@ -3,12 +3,57 @@ import programs.Domineering_Simulation.trad_Minimax as tm
 import time
 
 
-def main():
+def depth_comparison(graph, board, player = 1, stop = 99):
+    depth = 1
+
+    found_end = False
+    while (found_end == False):
+        game = Minimax(AI_player=player, graph=graph, board=board, depth=depth)
+        game = game.winning_moves
+        if game != {}:
+            print('All moves found which result in a win condition board state: ', end=" ")
+            if player == 1:
+                print({k for k in game if game[k] > 0})
+                move_to_play = max(game, key=lambda v: game[v])
+                print(f"maximum value move: {move_to_play}")
+            else:
+                print({k for k in game if game[k] < 0})
+                move_to_play = min(game, key=lambda v: game[v])
+                print(f"minimum value move: {move_to_play}")
+
+            found_end = True
+        if depth >= stop:   # For larger boards we may way a stopping point to prevent long runs
+            break
+        depth += 1
+    print(f"\nDepth Wincon Minimax stopped at: {depth} \nDepth Traditional Minimax will use: {depth}")
+
+    second_game = tm.Minimax(AI_player=player, graph=graph, board=board, depth=depth)
+    moves = second_game.winning_moves
+    print(f"\nWinning moves found by traditional Minimax at same depth: {moves}")
+
+
+def time_comparison(graph, board, player, depth):
+    for depth in range(1, depth+1):
+        delta = time.time()
+        traditional_minimax = tm.Minimax(AI_player=player, graph=graph, board=board, depth=depth)
+        traditional_minimax_time = time.time() - delta
+
+        delta = time.time()
+        wincon_minimax = Minimax(AI_player=player, graph=graph, board=board, depth=depth)
+        wincon_minimax_time = time.time() - delta
+
+        print(f"\nTime comparison at recursive depth: {depth}\n"
+              f"Traditional Minimax: {traditional_minimax_time} \n"
+              f"Win Condition Minimax: {wincon_minimax_time}")
+
+
+
+
+def main(time_test = False, depth_test = False):
     file = 'graphs.txt'
     height = 5
     length = 5
     graph = generate_empty_graph(height=height, length=length)
-    print(graph)
     board = generate_empty_board(height, length)
     board = fill_board(file=file, board=board)
     build_graph_from_board(graph=graph, board=board)
@@ -16,34 +61,17 @@ def main():
     depth = 5
     player = 1
 
+    print(graph)
     if player == 1:
-        print('AI player is Vertical')
+        print('\nAI player is Vertical\n')
     else:
-        print('AI player is Horizontal')
-    for depth in range(1, depth+1):
-        delta = time.time()
-        traditional_minimax = tm.Minimax(AI_player=player, graph=graph, board=board, depth=depth)
-        # trad_wins = tm.Minimax(AI_player=-1, graph=graph, board=board, depth=depth).found_end
-        traditional_minimax_time = time.time() - delta
+        print('\nAI player is Horizontal\n')
 
-        delta = time.time()
-        wincon_minimax = Minimax(AI_player=player, graph=graph, board=board, depth=depth)
-        possible_first_moves = wincon_minimax.winning_moves
-        # wincon_wins = wincon_minimax.found_end
-        wincon_minimax_time = time.time() - delta
+    if time_test == True:
+        time_comparison(graph=graph, board=board, player=player, depth=depth)
 
-        print(f"\nTime comparison at recursive depth: {depth}\n"
-              f"Traditional Minimax: {traditional_minimax_time} \n"
-              f"Win Condition Minimax: {wincon_minimax_time}")
-
-        # print(f"\nNumber of solved games: \n"
-        #       f"Traditional Minimax: {trad_wins}\n"
-        #       f"Win Condition Minimax: {wincon_wins}")
-
-        filtered_list = {k for k in possible_first_moves if possible_first_moves[k] < 0}
-        # print(f"\nThe full list of moves and their values: {possible_first_moves} \n"
-        #       f"List of moves which guarantee AI the win: {filtered_list}")
-        print(f"Moves found by wincon Minimax which guarantee an AI win: {filtered_list}")
+    if depth_test == True:
+        depth_comparison(graph=graph, board=board, player=player)
 
 if __name__ == '__main__':
-    main()
+    main(time_test = False, depth_test = True)
